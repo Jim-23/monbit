973961185aa0aca560ceef310bfc3b7d7495b78d
Jesse Sadowý <jesse.sadowy23@gmail.com>
2026-05-31 17:11:35 +0200

    feat: display first Monbit screen

--- monbit/src/main.cpp
+++ monbit/src/main.cpp
@@ -1,15 +1,44 @@
 #include <Arduino.h>
 
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+// BUSY, RST, DC, CS
+GxEPD2_BW<GxEPD2_154_GDEY0154D67, GxEPD2_154_GDEY0154D67::HEIGHT> display(
+    GxEPD2_154_GDEY0154D67(5, 4, 3, 2));
+
 void setup()
 {
     Serial.begin(115200);
-    delay(2000);
-
     Serial.println("=== MONBIT BOOTS ===");
+
+    SPI.begin(6, -1, 7, 5);
+
+    display.init(115200);
+
+    display.setRotation(0);
+
+    display.setFullWindow();
+    display.firstPage();
+
+    do
+    {
+        display.fillScreen(GxEPD_WHITE);
+
+        display.setFont(&FreeMonoBold9pt7b);
+        display.setTextColor(GxEPD_BLACK);
+
+        display.setCursor(20, 50);
+        display.print("MONBIT");
+
+        display.setCursor(20, 80);
+        display.print("v0.0.1");
+    }
+    while (display.nextPage());
+
+    Serial.println("Display updated");
 }
 
 void loop()
 {
-    Serial.println("ALIVE");
-    delay(1000);
 }
